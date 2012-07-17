ca866a21230a0f0c93c759f096e4852fa03496f0
Daniel Steuernol <dlsteuer@gmail.com>
2012-07-17 06:40:03 -0700

    Partial commit of TetrominoTests

---
+++ Tests/RotatingTetrominosTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+namespace Tetris.Tests
+{
+    public class RotatingTetrominosTests
+    {
+        [TestFixture]
+        public class All_Shape_Instances : TetrominoTest
+        {
+            [SetUp]
+            public void CreateAnyShape()
+            {
+                _shape = Tetromino.T_SHAPE;
+            }
+
+            [Test]
+            public void Are_Immutable()
+            {
+                var original = _shape.ToString();
+                _shape.RotateRight();
+                Assert.That(original, Is.EqualTo(_shape.ToString()));
+                _shape.RotateLeft();
+                Assert.That(original, Is.EqualTo(_shape.ToString()));
+            }
+        }
+
+        [TestFixture]
+        public class The_T_Shape : TetrominoTest
+        {
+            [SetUp]
+            public void CreateTShape()
+            {
+                _shape = Tetromino.T_SHAPE;
+            }
+
+            [Test]
+            public void Is_Shaped_Like_T()
+            {
+                Assert.That("" +
+                    ".T.\n" +
+                    "TTT\n" +
+                    "...\n", Is.EqualTo(_shape.ToString()));
+            }
+        }
+    }
+
+    public class TetrominoTest
+    {
+        protected Tetromino _shape;
+    }
+}
