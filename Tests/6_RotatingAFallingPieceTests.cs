3932ec52ea05f15bf44ac0fa39a013c6505cbc38
Daniel Steuernol <dlsteuer@gmail.com>
2012-07-17 08:57:54 -0700

    Final Tests

---
+++ Tests/6_RotatingAFallingPieceTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Tetris.Tests
+{
+    public class RotatingAFallingPieceTests
+    {
+        // TODO: a falling piece can be rotated clockwise
+        // TODO: a falling piece can be rotated counter-clockwise
+        // TODO: it can not be rotated when there is no room to rotate (left wall, right wall, other pieces...)
+
+        // TODO: when piece is up against a wall (or piece) and it is rotated (no room to rotate), move it away from the wall ("wallkick")
+        // See: http://bsixcentdouze.free.fr/tc/tgm-en/tgm.html
+        // http://bsixcentdouze.free.fr/tc/tgm-en/img/wallkick1.png
+        // http://bsixcentdouze.free.fr/tc/tgm-en/img/wallkick2.png
+        // http://bsixcentdouze.free.fr/tc/tgm-en/img/wallkick3.png
+    }
+}
